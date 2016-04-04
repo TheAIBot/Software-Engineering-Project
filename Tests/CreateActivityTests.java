31ccd656899b865362cb0255c23ebdbb4a09c30f
Lombre <jesper.birch3@gmail.com>
2016-04-04 19:26:20 +0200

    Added basic implimentation of the use case for add activity + basic test - still untested, requires project

--- Tests/CreateActivityTests.java
+++ Tests/CreateActivityTests.java
@@ -1,14 +1,36 @@
 
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
-
 import org.junit.Test;
 
+import com.sun.swing.internal.plaf.metal.resources.metal;
+
 public class CreateActivityTests {
+	
+	@Test
+	public void testCreateNormal() {				
+		Project project = new Project("Navision Stat");
+		assertTrue(project.getOpenActivites.size() == 0);
+		String activityName = "Udvikling af brugerinterface";
+		String activityDetailedDescription = "oprettelsen af et brugerinterface for programmet";
+		int excepctedHours = 200;
+		Calendar startTime = new GregorianCalendar();
+		startTime.set(2016, 3, 16);
+		Calendar endTime = new GregorianCalendar();
+		endTime.set(2016, 4, 18);
+		project.addAcitivity(activityName,	activityDetailedDescription, 200, startTime, endTime);
+		assertTrue(project.getOpenActivites.size() == 1);
+		Activity activity = project.getOpenActivities.get(0);
+		assertTrue(activity.getName() == activityName);
+		assertTrue(activity.getDescription() == activityDetailedDescription);
+		assertTrue(activity.getBudgetedTime() == excepctedHours);
+		assertTrue(activity.getTimePeriod().getStartDate() == startTime);
+		assertTrue(activity.getTimePeriod().getEndDate() == endTime);		
+	}
+	
 	
 }
