4700840c600f9813ecd65e19ffea62998689d089
Lombre <jesper.birch3@gmail.com>
2016-04-04 16:46:44 +0200

    meh, nothing

--- Tests/CreateActivityTests.java
+++ Tests/CreateActivityTests.java
@@ -1,4 +1,14 @@
 
-public class CreateActivityTests {
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class CreateActivityTests {
+	
 }
