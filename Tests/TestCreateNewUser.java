f7f67f6a6b58955af8df0c7d85da57995f0daf84
FuntasticFour <niklasr95@gmail.com>
2016-04-04 17:00:59 +0200

    Test case "TestCreateNewUser" created.

--- Tests/TestCreateNewUser.java
+++ Tests/TestCreateNewUser.java
@@ -5,9 +5,18 @@ import org.junit.Test;
 
 public class TestCreateNewUser {
 
+
+	/**
+	 * Tests if a User can succesfully be created with the given initials.
+	 * 
+	 * @author Niklas Refsgaard
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testUserConstruction(){
+		String initials = "test";
+		User user = new User(initials);
+		assertEquals(user.getInitials(),initials);
+		
 	}
 	
 	
