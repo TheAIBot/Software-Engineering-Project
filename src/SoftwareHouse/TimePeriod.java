dabfe91522b7d2e555b060af971055625f6a85fe
TheAIBot <andreasuranus@hotmail.com>
2016-04-05 11:46:08 +0200

    herp a derp

--- src/SoftwareHouse/TimePeriod.java
+++ src/SoftwareHouse/TimePeriod.java
@@ -1,5 +1,34 @@
 package SoftwareHouse;
+import java.sql.Date;
+import java.util.Calendar;
 
-public class TimePeriod {
+
+public class Timeperiod {
+	
+	private Calendar startDate;
+	private Calendar endDate;
+	
+	public Timeperiod(Calendar starteDate, Calendar endDate) {
+		this.setStartDate(starteDate);
+		this.setEndDate(endDate);
+	}
+
+	public Calendar getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Calendar startDate) {
+		this.startDate = startDate;
+	}
+
+	public Calendar getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Calendar endDate) {
+		this.endDate = endDate;
+	}
+	
+	
 
 }
