aa1130453c34118813dd2f20de88566cc1eb744f
TheAIBot <andreasuranus@hotmail.com>
2016-04-06 03:04:30 +0200

    added delete activity and add employee to project

--- Tests/DeleteActivity.java
+++ Tests/DeleteActivity.java
@@ -3,6 +3,9 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -11,8 +14,7 @@ import org.junit.Test;
 
 import SoftwareHouse.Project;
 import SoftwareHouse.Scheduler;
-import SoftwareHouse.ExceptionTypes.DuplicateProjectNameException;
-import SoftwareHouse.ExceptionTypes.NoNameException;
+import SoftwareHouse.ExceptionTypes.ActivityNotFoundException;
 
 public class DeleteActivity {
 
@@ -21,12 +23,76 @@ public class DeleteActivity {
 	{
 		Scheduler scheduler = new Scheduler();
 		try {
-			scheduler.createProject("Derp");
+			scheduler.createProject("Navision Stat");
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		Project project = scheduler.getProject("Derp");
-		project.addActivity();
-		project.deleteActivity();
+		
+		Project project = null;
+		try {
+			project = scheduler.getProject("Navision Stat");
+		} catch (Exception e) {
+			Assert.fail();
+		}
+		assertEquals(project.getOpenActivities().size(), 0);
+		
+		String activityName = "Udvikling af brugerinterface";
+		String activityDetailedDescription = "oprettelsen af et brugerinterface for programmet";
+		int expectedHours = 200;
+		Calendar startDate = new GregorianCalendar();
+		startDate.set(2016, 3, 16);
+		Calendar endDate = new GregorianCalendar();
+		endDate.set(2016, 4, 18);
+		try {
+			scheduler.addEmployee("JBS");
+			scheduler.addEmployee("ELL");
+			scheduler.addEmployee("AGC");
+			scheduler.addEmployee("NR");
+		} catch (Exception e) {
+			Assert.fail();
+		}
+		List<String> employeeInitials = new ArrayList<String>();
+		employeeInitials.add("JBS");
+		employeeInitials.add("ELL");
+		employeeInitials.add("AGC");
+		employeeInitials.add("NR");
+		
+		try {
+			project.addAcitivity(activityName,	activityDetailedDescription, employeeInitials, startDate, endDate, expectedHours);
+		} catch (Exception e) {
+			Assert.fail();
+		}
+		
+		assertEquals(project.getOpenActivities().size(), 1);
+		assertEquals(project.getDeletedActivities().size(), 0);
+		try {
+			project.deleteActivity(activityName);
+		} catch (ActivityNotFoundException e) {
+			Assert.fail();
+		}
+		assertEquals(project.getOpenActivities().size(), 0);
+		assertEquals(project.getDeletedActivities().size(), 1);
+	}
+
+	public void DeleteActivityMissingActivitytest()
+	{
+		Scheduler scheduler = new Scheduler();
+		try {
+			scheduler.createProject("Navision Stat");
+		} catch (Exception e) {
+			Assert.fail();
+		}
+		
+		Project project = null;
+		try {
+			project = scheduler.getProject("Navision Stat");
+		} catch (Exception e) {
+			Assert.fail();
+		}
+		try {
+			project.deleteActivity("Does not exist");
+			Assert.fail();
+		} catch (ActivityNotFoundException e) {
+		}
 	}
 }
