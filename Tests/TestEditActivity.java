b7b265b6ab321625c0d4595443f95037ed3451c3
Dragerytteren <dragerytteren@gmail.com>
2016-04-18 17:01:52 +0200

    Added TestEditAcitivty (almost done)

---
+++ Tests/TestEditActivity.java
@@ -0,0 +1,91 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import SoftwareHouse.Activity;
+import SoftwareHouse.Project;
+import SoftwareHouse.Scheduler;
+import SoftwareHouse.ExceptionTypes.ActivityNotFoundException;
+import SoftwareHouse.ExceptionTypes.EmployeeNotFoundException;
+import SoftwareHouse.ExceptionTypes.MissingProjectException;
+import SoftwareHouse.ExceptionTypes.ProjectNotFoundException;
+
+public class TestEditActivity {
+	
+	Scheduler scheduler;
+	
+	@Before
+	public void setup() {
+		Scheduler scheduler = new Scheduler();
+		
+		// Create employees
+		try {
+			scheduler.addEmployee("AGC");
+			scheduler.addEmployee("ELL");
+		} catch (Exception e) {
+			Assert.fail();
+		}
+		
+		// Create project
+		try {
+			scheduler.createProject("Navision Stat");
+		} catch (Exception e) {
+			Assert.fail();
+		}
+		Project project = null;
+		try {
+			project = scheduler.getProject("Navision Stat");
+		} catch (MissingProjectException e1) {
+			Assert.fail();
+		}
+		try {
+			project.addEmployee("AGC");
+			project.addEmployee("ELL");
+		} catch (EmployeeNotFoundException e) {
+			Assert.fail();
+		}
+		
+		// Create activity
+		String activityName = "Brugerinterface";
+		String activityDetailedDescription = "Udvikling af brugerinterface";
+		int expectedHours = 150;
+		Calendar startDate = new GregorianCalendar();
+		startDate.set(2016, 3, 16);
+		Calendar endDate = new GregorianCalendar();
+		endDate.set(2016, 4, 18);
+		List<String> employeeInitials = new ArrayList<String>();
+		employeeInitials.add("AGC");
+		employeeInitials.add("ELL");
+		try {
+			project.addAcitivity(activityName,	activityDetailedDescription, employeeInitials, startDate, endDate, expectedHours);
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testEditActivitySucces() {
+		Activity activity = null;
+		try {
+			activity = scheduler.getActivity("Navision Stat", "Brugerinterface");
+		} catch (ProjectNotFoundException e) {
+			e.printStackTrace();
+		} catch (ActivityNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		assertNotNull(activity);
+		assertEquals(activity.getBudgettedTime(), 150);
+		activity.setBudgettedTime(275);
+		assertEquals(activity.getBudgettedTime(), 275);
+		
+	}
+	
+}
