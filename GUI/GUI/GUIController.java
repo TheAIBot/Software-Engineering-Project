3f0dec2e8f13519f3d5f55fedf9f583d53ce9080
TheAIBot <andreasuranus@hotmail.com>
2016-05-09 09:36:49 +0200

    added another way to load the GUI with preloaded scheduler information

--- GUI/GUI/GUIController.java
+++ GUI/GUI/GUIController.java
@@ -15,60 +15,32 @@ import junit.framework.Test;
 
 public class GUIController {
 	private JFrame frame;
-	private final Scheduler scheduler = new Scheduler();
-	private final MainPage mainPage = new MainPage(this, scheduler);
-	public final LoginPage loginPage = new LoginPage(this, scheduler);
-	public final ProjectsPage projectPage = new ProjectsPage(this, scheduler);
-	public final MyPagePage myPagePage = new MyPagePage(this, scheduler);
+	private final Scheduler scheduler;
+	private final MainPage mainPage;
+	public final LoginPage loginPage;
+	public final ProjectsPage projectPage;
+	public final MyPagePage myPagePage;
 	
  	public GUIController()
 	{
-		frame = new JFrame("herp a derp");
-		frame.add(mainPage.getPage(this));
-		frame.setSize(1010, 640); 
+ 		this(new Scheduler());
+	}
+ 	
+ 	public GUIController(Scheduler scheduler)
+	{
+ 		this.scheduler = scheduler;
+ 		this.mainPage = new MainPage(this, scheduler);
+ 		this.loginPage = new LoginPage(this, scheduler);
+ 		this.projectPage = new ProjectsPage(this, scheduler);
+ 		this.myPagePage = new MyPagePage(this, scheduler);
+ 		
+		this.frame = new JFrame("üëÅ Big Brother");
+		this.frame.add(mainPage.getPage(this));
+		this.frame.setSize(1010, 640); 
 	}
 	
 	public void show()
-	{
-		try {
-			Tests.TestTools.addEmployee(scheduler, "AGC");
-			Tests.TestTools.addEmployee(scheduler, "JSB");
-			Tests.TestTools.addEmployee(scheduler, "NR");
-			Tests.TestTools.addEmployee(scheduler, "ELL");
-			
-			Tests.TestTools.login(scheduler);
-			
-			Tests.TestTools.createProject(scheduler, "Do the things");
-			Tests.TestTools.createProject(scheduler, "For the lols");
-			Tests.TestTools.createProject(scheduler, "Test project please ignore");
-			Tests.TestTools.createProject(scheduler, "Lave pizza");
-			Tests.TestTools.createProject(scheduler, "FÂ 12");
-			Tests.TestTools.createProject(scheduler, "Zing", "ELL");
-			Tests.TestTools.createProject(scheduler, "import AI.Skynet;");
-			Tests.TestTools.safeCloseProject(scheduler, "import AI.Skynet;");
-			
-			Tests.TestTools.addEmployeeToProject(scheduler, Tests.TestTools.LOGIN_EMPLOYEE_INITIALS, "Zing");
-			Tests.TestTools.addEmployeeToProject(scheduler, Tests.TestTools.LOGIN_EMPLOYEE_INITIALS, "Lave pizza");
-			Tests.TestTools.addEmployeeToProject(scheduler, Tests.TestTools.LOGIN_EMPLOYEE_INITIALS, "FÂ 12");
-			
-			scheduler.login("ELL");
-			Tests.TestTools.addActivity(scheduler, "Zing", "Zang", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
-			Tests.TestTools.addActivity(scheduler, "Zing", "Ding", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
-			Tests.TestTools.addActivity(scheduler, "Zing", "Dong", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
-			Tests.TestTools.addActivity(scheduler, "Zing", "Pling", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
-			Tests.TestTools.addActivity(scheduler, "Zing", "Plong", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
-			scheduler.login("LLLL");
-			Tests.TestTools.addActivity(scheduler, "Lave pizza", "Ost", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
-			Tests.TestTools.addActivity(scheduler, "Lave pizza", "Pepperoni", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
-			Tests.TestTools.addActivity(scheduler, "Lave pizza", "Tomat", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
-			Tests.TestTools.addActivity(scheduler, "Lave pizza", "Dej", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
-			Tests.TestTools.addActivity(scheduler, "Lave pizza", "Skinke", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
-			
-		} catch (Exception e) 
-		{
-			e.printStackTrace();
-		}
-		
+	{		
 		switchPage(loginPage);
 	}
 	
---
+++ GUI/GUIPreloadedInfo/PreloadScheduler.java
@@ -0,0 +1,51 @@
+package GUIPreloadedInfo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import GUI.GUIController;
+import SoftwareHouse.Scheduler;
+
+public class PreloadScheduler {
+	
+	public static void main(String[] args) {
+		try {
+			Scheduler scheduler = new Scheduler();
+			
+			Tests.TestTools.addEmployee(scheduler, "AGC");
+			Tests.TestTools.addEmployee(scheduler, "JSB");
+			Tests.TestTools.addEmployee(scheduler, "NR");
+			Tests.TestTools.addEmployee(scheduler, "ELL");
+			
+			Tests.TestTools.login(scheduler);
+			
+			Tests.TestTools.createProject(scheduler, "Do the things");
+			Tests.TestTools.createProject(scheduler, "For the lols");
+			Tests.TestTools.createProject(scheduler, "Test project please ignore");
+			Tests.TestTools.createProject(scheduler, "Lave pizza");
+			Tests.TestTools.createProject(scheduler, "Få 12");
+			Tests.TestTools.createProject(scheduler, "Zing", "ELL");
+			Tests.TestTools.createProject(scheduler, "import AI.Skynet;");
+			Tests.TestTools.safeCloseProject(scheduler, "import AI.Skynet;");
+			
+			Tests.TestTools.addEmployeeToProject(scheduler, Tests.TestTools.LOGIN_EMPLOYEE_INITIALS, "Zing");
+			Tests.TestTools.addEmployeeToProject(scheduler, Tests.TestTools.LOGIN_EMPLOYEE_INITIALS, "Lave pizza");
+			Tests.TestTools.addEmployeeToProject(scheduler, Tests.TestTools.LOGIN_EMPLOYEE_INITIALS, "Få 12");
+			
+			scheduler.login("ELL");
+			Tests.TestTools.addActivity(scheduler, "Zing", "Zang", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
+			Tests.TestTools.addActivity(scheduler, "Zing", "Ding", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
+			Tests.TestTools.addActivity(scheduler, "Zing", "Dong", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
+			Tests.TestTools.addActivity(scheduler, "Zing", "Pling", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
+			Tests.TestTools.addActivity(scheduler, "Zing", "Plong", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
+			scheduler.login("LLLL");
+			Tests.TestTools.addActivity(scheduler, "Lave pizza", "Ost", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
+			Tests.TestTools.addActivity(scheduler, "Lave pizza", "Pepperoni", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
+			Tests.TestTools.addActivity(scheduler, "Lave pizza", "Tomat", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
+			Tests.TestTools.addActivity(scheduler, "Lave pizza", "Dej", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
+			Tests.TestTools.addActivity(scheduler, "Lave pizza", "Skinke", new String[] {Tests.TestTools.LOGIN_EMPLOYEE_INITIALS});
+			
+			new GUIController(scheduler).show();		
+		} catch (Exception e) {	}
+	}
+}
